4067e29121f9c93ecda1741be30fcfb85c37ba69
Nils Sturma <nils.sturma@tum.de>
2020-11-19 17:08:52 +0100

    cleaned simulate-sizes.R

--- simulations/simulate-sizes.R
+++ simulations/simulate-sizes.R
@@ -4,97 +4,56 @@ library(MASS) #mvrnorm
 library(igraph)
 library(TestGLTM)
 
-#setwd("src/general-setup")
-
-#################
-# Create a tree #
-#################
-
-# TODO: put the following three functions in the package
-
-# Create an igraph object that is the quinted tree
-quinted_tree <- function(){
-  colors <- c("tomato", "gray50")
-  vertices <- data.frame(name=seq(1,8),
-                       type=c(rep(1,5), rep(2,3)), # 1=observed, 2=latent -> always first m nodes should be observed (=leaves)
-                       color=colors[c(1,1,1,1,1,2,2,2)])
-  edges <- data.frame(from=c(1,2,3,4,5,6,7), to=c(8,8,6,6,7,7,8))
-  g <- graph_from_data_frame(edges, directed=FALSE, vertices=vertices)
-  return(g)
-}
-
-
-# Create an igraph object that is star tree
-star_tree <- function(m=10){
-  colors <- c("tomato", "gray50")
-  vertices <- data.frame(name=seq(1,m+1), 
-                         type=c(rep(1,m), 2), # 1=observed, 2=latent -> always first m nodes should be observed (=leaves)
-                         color=colors[c(rep(1,m), 2)])
-  edges <- data.frame(from=seq(1,m), to=rep((m+1),m))
-  g <- graph_from_data_frame(edges, directed=FALSE, vertices=vertices)
-  return(g)
-}
-
-# Sample covariance matrix from star tree depending on setup
-cov_from_star_tree <- function(g, setup=1, m=10){
-  if (setup==1){
-    V(g)$var = rep(2,m+1)
-    # for unobserved node: variance does not matter
-    E(g)$corr = rep(sqrt(0.5),m)
-    cov = cov_from_graph(g)
-  } else if (setup==2){
-    beta = c(10,10, rnorm((m-2),0,0.2))
-    V(g)$var = c(beta**2+rep(1/3,m),1)
-    E(g)$corr = beta / sqrt(beta**2+rep(1/3,m))
-    cov = cov_from_graph(g)
-  }
-  return(cov)
-}
-
-
-
-
-
-
-
+setwd("../simulations")
+source("utils.R")
 
 #################
 # Set variables #
 #################
 
-# Set variables
+# General
 n = 1000
-m = 20
 E = 1000
-setup = 1
-alphas = seq(0.01, 0.99, 0.01)
 nr_exp = 500
+alphas = seq(0.01, 0.99, 0.01)
 
+
+# Test strategy
 test_strategy="symmetric"  # "run-over", "grouping", "two-step", "symmetric"
 B = 5  # just for test_strategy=="run-over" (5 works best for setup 1 after doing some experiments)
 beta = 0.001  # just for test_strategy=="two-step"
 
+# Tree
+tree = "binary_rooted"  # "star_tree", "quinted_tree", "binary_rooted"
+m = 20  # (star_tree)
+setup = 2  # (star_tree)
+
+
+
+# Saving
 save=FALSE
 name = paste(format(Sys.time(), "%Y-%m-%d-%H-%M"), "_", "quinted-tree_", method, "_n=", n, sep="")
 
-################
-# Create graph #
-################
-g = star_tree(m)
-#plot(g)
-# g = quinted_tree()
-# plot(g)
-# V(g)$var = rep(2,8)
-# E(g)$corr = rep(0.5,7)
-# cov = cov_from_graph(g)
 
 
-# Collect indices
+###################################
+# Create tree and collect indices #
+###################################
+
+if (tree=="star_tree"){
+  g = star_tree(m)
+} else if (tree=="binary_rooted"){
+  g = binary_rooted()
+} else if (tree=="quinted_tree"){
+  g = quinted_tree()
+}
+
+plot(g)
+
 res = collect_indices(g)
 ind_eq = matrix(unlist(res[[1]]), ncol = 8, byrow = TRUE)
 ind_ineq1 = matrix(unlist(res[[2]]), ncol = 6, byrow = TRUE)
 ind_ineq2 = matrix(unlist(res[[3]]), ncol = 8, byrow = TRUE)
-
 
 
 
@@ -124,7 +83,18 @@ results <- foreach(nr = 1:nr_exp, .combine=rbind, .packages=c("MASS", "TestGLTM"
   warnings()
   
   # Generate n independent data sets depending on setup
-  cov = cov_from_star_tree(g, setup=setup, m=m)
+  if (tree=="star_tree"){
+    cov = cov_from_star_tree(g, setup=setup, m=m)
+  } else if (tree=="binary_rooted"){
+    V(g)$var = rep(2,22)
+    E(g)$corr = rep(0.5,21)
+    cov = cov_from_graph(g)
+  } else if (tree=="quinted_tree"){
+    V(g)$var = rep(2,8)
+    E(g)$corr = rep(0.5,7)
+    cov = cov_from_graph(g)
+  }
+ 
   X = mvrnorm(n, mu=rep(0,nrow(cov)), Sigma=cov)
   
   # Call the test
@@ -169,14 +139,3 @@ abline(coef = c(0,1))
 if (save){
   dev.off() # close pdf file
 }
-
-
-
-##########
-# Others #
-##########
-
-# # Random multivariate normal distribution
-# A = matrix(rnorm(25,0,1), nrow=5)
-# Sigma = A %*% t(A)
-# X = mvrnorm(500, mu=rep(0,5), Sigma=Sigma)
---
+++ simulations/utils.R
@@ -0,0 +1,63 @@
+library(TestGLTM)
+
+
+
+# Create an igraph object that is the quinted tree
+quinted_tree <- function(){
+  colors <- c("tomato", "gray50")
+  vertices <- data.frame(name=seq(1,8),
+                         type=c(rep(1,5), rep(2,3)), # 1=observed, 2=latent -> always first m nodes should be observed (=leaves)
+                         color=colors[c(1,1,1,1,1,2,2,2)])
+  edges <- data.frame(from=c(1,2,3,4,5,6,7), to=c(8,8,6,6,7,7,8))
+  g <- graph_from_data_frame(edges, directed=FALSE, vertices=vertices)
+  return(g)
+}
+
+
+
+# Create an igraph object that is a binar rooted tree with 12 observed nodes (i.e 10 hidden nodes, 21 edges)
+binary_rooted <- function(){
+  colors <- c("tomato", "gray50")
+  name = seq(1,22)
+  type = c(rep(1,12), rep(2,10))
+  vertices <- data.frame(name=name, 
+                         type=type, # 1=observed, 2=latent -> always first m nodes should be observed (=leaves)
+                         color=colors[type])
+  edges <- data.frame(from=c(22,22,22,19,19,20,20,21,21,13,13,14,14,15,15,16,16,17,17,18,18), 
+                      to=c(19,20,21,13,14,15,16,17,18,1,2,3,4,5,6,7,8,9,10,11,12))
+  g <- graph_from_data_frame(edges, directed=FALSE, vertices=vertices)
+  return(g)
+}
+
+
+
+# Create an igraph object that is star tree
+star_tree <- function(m=10){
+  colors <- c("tomato", "gray50")
+  name = seq(1,m+1)
+  type = c(rep(1,m), 2)
+  vertices <- data.frame(name=name, 
+                         type=type, # 1=observed, 2=latent -> always first m nodes should be observed (=leaves)
+                         color=colors[type])
+  edges <- data.frame(from=seq(1,m), to=rep((m+1),m))
+  g <- graph_from_data_frame(edges, directed=FALSE, vertices=vertices)
+  return(g)
+}
+
+
+
+# Sample covariance matrix from star tree depending on setup
+cov_from_star_tree <- function(g, setup=1, m=10){
+  if (setup==1){
+    V(g)$var = rep(2,m+1)
+    # for unobserved node: variance does not matter
+    E(g)$corr = rep(sqrt(0.5),m)
+    cov = cov_from_graph(g)
+  } else if (setup==2){
+    beta = c(10,10, rnorm((m-2),0,0.2))
+    V(g)$var = c(beta**2+rep(1/3,m),1)
+    E(g)$corr = beta / sqrt(beta**2+rep(1/3,m))
+    cov = cov_from_graph(g)
+  }
+  return(cov)
+}
