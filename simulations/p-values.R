b7a0f02941c53e6e164e0a2e9f4adeeae53771a9
Nils Sturma <nils.sturma@web.de>
2021-04-08 10:08:09 +0200

    simulated p-values for LR test

--- simulations/p-values.R
+++ simulations/p-values.R
@@ -13,18 +13,17 @@ setwd("/dss/dsshome1/lxc0D/ge73wex3/master-thesis-tests")
 # General
 n_range = c(500)
 E = 1000
-nr_exp = 500
-alphas = seq(0.01, 0.99, 0.01)
+nr_exp = 5000
+
 
 # Test strategy
 strategies = c("LR")  # Possible: "grouping", "run-over", "U-stat", "LR", "grouping-cov", "run-over-cov"
-B = 5  # only relevant if test_strategy=="run-over" 
 N = 5000  # only relevant if test_strategy=="U-stat"
 
 # Tree
 tree = "star_tree"  # Possible: "star_tree", "cat_binary"
 m = 20
-setup = 2  # only relevant if tree=="star_tree"
+setup = 1  # only relevant if tree=="star_tree"
 
 # High dimensionality?
 nr_4 = NULL  # 5000, NULL
@@ -36,7 +35,8 @@ only_equalities = FALSE
 # Saving
 save=TRUE
 
-
+# Number of cores used in computation
+cores = 20 #detectCores()
 
 ###################################
 # Create tree and collect indices #
@@ -55,20 +55,20 @@ plot(g)
 # Save all paths between all nodes in the tree (doing this just once reduces computational time)
 paths = get_paths(g)
 
-# Collect the representations of the polynomials that have to be tested
-res = collect_indices(g, m, nr_4, nr_3)
-ind_eq = res$ind_eq
-ind_ineq1 = res$ind_ineq1
-ind_ineq2 = res$ind_ineq2
-p = dim(ind_eq)[1] + dim(ind_ineq1)[1] + dim(ind_ineq2)[1]
-if (only_equalities){
-  ind_ineq1 = NULL
-  ind_ineq2 = NULL
-  p = dim(ind_eq)[1]
-}
-
-# Check the dimension
-print(p)
+# # Collect the representations of the polynomials that have to be tested
+# res = collect_indices(g, m, nr_4, nr_3)
+# ind_eq = res$ind_eq
+# ind_ineq1 = res$ind_ineq1
+# ind_ineq2 = res$ind_ineq2
+# p = dim(ind_eq)[1] + dim(ind_ineq1)[1] + dim(ind_ineq2)[1]
+# if (only_equalities){
+#   ind_ineq1 = NULL
+#   ind_ineq2 = NULL
+#   p = dim(ind_eq)[1]
+# }
+# 
+# # Check the dimension
+# print(p)
 
 
 
@@ -83,7 +83,6 @@ for (test_strategy in strategies){
     print(paste("n=",n ,sep=""))
     
     # Initialize the cluster
-    cores = 20 #detectCores()
     cl <- makeCluster(cores, outfile = "")
     registerDoParallel(cl)
     
@@ -94,7 +93,7 @@ for (test_strategy in strategies){
                        .packages=c("MASS", "TestGLTM", "igraph", "stats")) %dopar% {
       
       # Print some info
-      if((nr%%10) == 0){print(nr)}
+      if((nr%%100) == 0){print(nr)}
       warnings()
       
       # Generate n independent data sets depending on setup
@@ -127,9 +126,8 @@ for (test_strategy in strategies){
       }
       
       
-      # Rejected?
-      result = res$PVAL <= alphas # result: TRUE = rejected
-      result = as.numeric(result)
+      # p-value
+      pval = res$PVAL
     }
     
     # Check if every task was successful
@@ -137,44 +135,28 @@ for (test_strategy in strategies){
       print(paste("ERROR - ", (nr_exp-dim(results)[1]), " tasks where not succesful.", sep=""))
     }
     
-    # Compute the emprical sizes
-    sizes = colMeans(results)
     
     # Stop the cluster
     stopCluster(cl)
     
+    
     # Plot and save results
-    # Create meta information for the plot and the files to be saved
-    if (only_equalities){
-      prefix = "only_equalities"
-    } else {
-      prefix = format(Sys.time(), "%Y-%m-%d-%H-%M")
-    }
     if (tree=="star_tree"){
-      name = paste(prefix, "_", "star-tree_setup=", setup, "_n=", n, "_m=", m, sep="")
-      subtitle = paste("Star tree n=", n, " m=", m," strategy=", test_strategy, sep="")
-      title = paste("Emprical test sizes vs. nominal test levels based on ", nr_exp, " experiments. \n Star tree - setup ", setup, sep="")
+      name = paste("setup=", setup, "_n=", n, "_m=", m, "_exp=", nr_exp, sep="")
     } else if (tree=="cat_binary"){
-      name = paste(prefix, "_", "caterpillar", "_n=", n,  "_m=", m, sep="")
-      subtitle = paste("Caterpillar tree n=", n, " m=", m," strategy=", test_strategy, sep="")
-      title = paste("Emprical test sizes vs. nominal test levels based on ", nr_exp, " experiments. \n Caterpillar tree", sep="")
+      name = paste("n=", n, "_m=", m, "_exp=", nr_exp, sep="")
     }
     
-    # Plot & save
+    main = paste("Histogram of ", nr_exp, " simulated p-values", sep="")
+    
     if (save){
-      # use "./img/name.png" to save in subdirectory
-      name_pdf = paste("./results/", tree, "/", test_strategy, "/sizes/", name, ".pdf", sep="")
-      name_rds = paste("./results/", tree, "/", test_strategy, "/sizes/", name, ".rds", sep="")
-      saveRDS(sizes, file = name_rds) # read with readRDS()
-      pdf(name_pdf) # create pdf file
+      name_pdf = paste("./results/", tree, "/", test_strategy, "/p-values/", name, ".pdf", sep="")
+      name_rds = paste("./results/", tree, "/", test_strategy, "/p-values/", name, ".rds", sep="")
+      saveRDS(results, file = name_rds) 
+      pdf(name_pdf, width=8, height=8)
     }
-    plot(alphas, sizes, 
-         xlab="Nominal level", ylab="Emprical test size", main=title, sub=subtitle,
-         type="p", pch=1)
-    abline(coef = c(0,1))
-    if (save){
-      dev.off() # close pdf file
-    }
+    hist(results, breaks=seq(0,1,0.05), xlab="p-value", ylab="", freq=FALSE, main="") 
+    if (save){dev.off()}
   }
 }
 
